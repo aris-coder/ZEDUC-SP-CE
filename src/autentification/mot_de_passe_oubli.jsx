c7c02494d3a310ed64c8beea24d45886ef0df661
Reine Foguem <reinefoguem@gmail.com>
2024-10-16 08:44:22 +0100

    séparation de la partie <style><style/> et de la div de la partie en noir pour creer des composants appelés dans connexion, inscription et mot de passe oublié. supression de autentificationStyle.css

--- src/autentification/mot_de_passe_oubli.jsx
+++ src/autentification/mot_de_passe_oubli.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
-import logo from '../images/logo.png';
-import kok from '../images/kok.png';
-import dg from '../images/dg.png';
+import Sidebar from './partienoir';
+import Style from './style';
 import pouletpanee from '../images/pouletpanee1.png';
 
 const MotDePasseOublie = () => {
@@ -16,58 +15,10 @@ const MotDePasseOublie = () => {
 
   return (
     <div className="container-fluid">
-      <style>
-        {`
-            @import url('https://fonts.googleapis.com/css2?family=Milonga&display=swap');
-            .form-label {
-                margin-bottom: 1px;
-            }
-            .form-control {
-                margin-top: 10px;
-            }
-            .password-eye {
-                cursor: pointer;
-                position: absolute;
-                right: 20px;
-                top: 40px;
-                z-index: 2;
-            }
-            .custom-link {
-                color: #c2a87f;
-            }
-            @media (max-width: 768px) {
-                .responsive-div {
-                    height: 20vh !important;
-                    }
-                .responsive-div img {
-                    width: 100px !important; 
-                    height: 100px !important;
-                }
-                .circle-img.kok {
-                    display: none;
-                }
-                .circle-img.dg {
-                    display: none;
-                }
-            }
-            .custom-link {
-                color: #c2a87f;
-            }
-        `}
-      </style>
+      <Style/>
 
       <div className="row">
-        <div className="col-md-4 responsive-div bg-black d-flex flex-column justify-content-between align-items-center py-4" style={{ height: '100vh', position: 'relative', overflow: 'hidden' }}>
-          <div className="circle-img kok">
-            <img src={kok} alt="cercle haut" className="rounded-circle" style={{ position: 'absolute', top: '-50px', left: '270px', width: '180px', height: '180px' }} />
-          </div>
-          <div>
-            <img src={logo} alt="logo" className="img-fluid mb-4" style={{ width: '200px' }} />
-          </div>
-          <div className="circle-img dg">
-            <img src={dg} alt="cercle bas" className="rounded-circle" style={{ position: 'absolute', top: '470px', left: '-20px', width: '180px', height: '180px' }} />
-          </div>
-        </div>
+        <Sidebar/>
 
         <div className="col-md-8 bg-white d-flex flex-column justify-content-center align-items-center py-4" style={{ fontFamily: 'Milonga, sans-serif', position: 'relative', overflow: 'hidden'}}>
           <div className="text-center mb-5">
