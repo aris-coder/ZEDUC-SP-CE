9a322f3998cddb91a579e844af1265e5c1352ab6
F/ARIS <aris.f.dev@gmail.com>
2024-10-20 19:55:42 +0100

    Page d'acceuil employé modifier selon le figma

--- OneDrive/Documents/PageEmployer/employer-react/src/Employer/AcceuilEmployer.jsx
+++ OneDrive/Documents/PageEmployer/employer-react/src/Employer/AcceuilEmployer.jsx
@@ -15,23 +15,66 @@ const AcceuilE = () => {
       <main>
         <img src={PlatI} class="img-fluid" className='PLATI'/>
         <img src={PlatII} class="img-fluid" className='PLATII'/>
-        <div className="bienvenue_message_employé">
+        <div className="bienvenue_message_employé" style={{fontSize:'45px',marginLeft:'90px',marginTop:'-40px'}}>
           ZEDUC SP@CE <br/> <br/>Bienvenue NOM 
+        </div>
+        <div className="container  menujour">
+          <br/>
+          <div className="row">
+            <div className="col">
+              <div style={{display: 'flex', justifyContent:'space-between' ,alignItems:'center',paddingLeft:'65px'}}>
+                <p style={{fontFamily:'Milonga, serif'}}>POULET PANET</p>
+                <p className='fw-bold' style={{fontFamily:'Be Vietnam Pro, serif',paddingRight:'58px',fontSize:'18px'}}>1000/1500</p>
+              </div>
+              <div className='ligne' style={{ borderBottom: '2px solid black'}}></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div style={{display: 'flex', justifyContent:'space-between' ,alignItems:'center',paddingLeft:'65px'}}>
+                <p style={{fontFamily:'Milonga, serif'}}>OKOK SUCRER</p>
+                <p className='fw-bold' style={{fontFamily:'Be Vietnam Pro, serif',paddingRight:'58px',fontSize:'18px'}}>1500</p>
+              </div>
+              <div className='ligne' style={{ borderBottom: '2px solid black'}}></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div style={{display: 'flex', justifyContent:'space-between' ,alignItems:'center',paddingLeft:'65px'}}>
+                <p style={{fontFamily:'Milonga, serif'}}>OKOK SALE</p>
+                <p className='fw-bold' style={{fontFamily:'Be Vietnam Pro, serif',paddingRight:'58px',fontSize:'18px'}}>1000/1500</p>
+              </div>
+              <div className='ligne' style={{ borderBottom: '2px solid black'}}></div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div style={{display: 'flex', justifyContent:'space-between' ,alignItems:'center',paddingLeft:'65px'}}>
+                <p style={{fontFamily:'Milonga, serif'}}>SAUCE JAUNE</p>
+                <p className='fw-bold' style={{fontFamily:'Be Vietnam Pro, serif',paddingRight:'58px',fontSize:'18px'}}>2000</p>
+              </div>
+              <div className='ligne' style={{ borderBottom: '2px solid black'}}></div>
+            </div>
+          </div>
+          <div  className="row">
+            <div className="col">
+              <div style={{display: 'flex', justifyContent:'space-between' ,alignItems:'center',paddingLeft:'65px'}}>
+                <p style={{fontFamily:'Milonga, serif'}}>ERU</p>
+                <p className='fw-bold' style={{fontFamily:'Be Vietnam Pro, serif',paddingRight:'58px',fontSize:'18px'}}>1000</p>
+              </div>
+              <div className='ligne' style={{ borderBottom: '2px solid black'}}></div>
+            </div>
+          </div>
         </div>
         <div class="commande_attente_info"></div>
         <div class="commande_attente">
-            Commandes en attente
-            <button class="voir_plus_employé">Voir plus</button>
+          Commandes en attente
+        <button class="voir_plus_employé">Voir plus</button>
         </div><br/><br/> 
-        <div class="plat_jour_info"></div>
-        <div class="plat_jour">
-            Plat du jour
-            <button class="voir_plusplat_employé">Voir plus</button>
-        </div><br/><br/>
         <div class="réclamations_info"></div>
         <div class="réclamation_en_attente">
-            Réclamation en attente
-            <button class="voir_plusréclamation_employé">Voir plus</button>
+          Réclamation en attente
+        <button class="voir_plusréclamation_employé">Voir plus</button>
         </div>
       </main>
       <footer style={{position:'absolute', top:'100%'}}>
